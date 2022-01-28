8e6dd4d9a1b1fc3e546333956a5b665c387d3d23
Mikael Bohlin Liljekvist <mikael0225@outlook.com>
2022-01-28 13:41:36 +0100

    Added functionallity to Deploy and undeploy for Clustered enviroments

--- BTDFDeploy-Arti/Deploy-BTDFApplication.ps1
+++ BTDFDeploy-Arti/Deploy-BTDFApplication.ps1
@@ -9,80 +9,103 @@ param(
     [Parameter(Mandatory=$true,HelpMessage="Path to the directory where the product is installed. EX: C:\Program Files (x86)")]
 	[string]$Destination,
 
+    [Parameter(Mandatory=$false,HelpMessage="Name of the cluster controlling bztalk. (Leave empty if target is not in cluster)")]
+	[string]$ClusterName,
+
+    [Parameter(Mandatory=$true,HelpMessage="Should this run on the last node? (False = Run on all nodes not running biztalk in the cluster, True = Run on all nodes with a active biztalk service.)")]
+	[string]$ShouldRunOnLastNode='true',
+
     [string]$BTDeployMgmtDB='true',
     [string]$SkipUndeploy='true'
 )
 . "$PSScriptRoot\Init-BTDFTasks.ps1"
 
+function IsLastNode {
+	if([string]::IsNullOrEmpty($ClusterName)){
+		return $true # This can be redone since this requires ShouldRunOnLastNode to be true
+	}
+	$OwnerNode = (Get-ClusterGroup -Name $ClusterName).OwnerNode
+	Write-Host $env:computername
+	Write-Host $OwnerNode
+	if($OwnerNode -eq $env:computername){
+		return $true
+	}
+	return $false
+}
+
 $RunOrderArray = $RunOrder.split(",")
 # "Shared", "INT", "LAB", "TEST"
 
-$path = "$Env:AGENT_RELEASEDIRECTORY";
+[System.Convert]::ToBoolean($ShouldRunOnLastNode);
 
-$files = Get-ChildItem $path;
+if(IsLastNode -eq $ShouldRunOnLastNode){
+    $path = "$Env:AGENT_RELEASEDIRECTORY";
 
-for(($i = 0); $i -lt $RunOrderArray.count; $i++){
+    $files = Get-ChildItem $path;
 
-	$CurrentWord = $RunOrderArray[$i];
+    for(($i = 0); $i -lt $RunOrderArray.count; $i++){
 
-    foreach ($Name in $files){
+        $CurrentWord = $RunOrderArray[$i];
 
-        if($Name.Contains($CurrentWord)){
-            $ApplicationPath = Join-Path $Destination $Name
-            Write-Host "Name: $Name, Environment: $Environment, Destination: $Destination, BTDeployMgmtDB: $BTDeployMgmtDB" 
+        foreach ($Name in $files){
 
-            if (Test-Path -Path $ApplicationPath -ErrorAction SilentlyContinue) {
-                if ($Environment)
-                {
-                    $EnvironmentSettingsPath = Get-ChildItem -Path $ApplicationPath -Recurse -Filter 'EnvironmentSettings' | Select-Object -ExpandProperty FullName -First 1
-                    $EnvironmentSettings = Join-Path $EnvironmentSettingsPath $Environment
-                    if ($Environment -notmatch '\.xml') {
-                        # This offers backwards compatibility for existing tasks which are set to the environment name, not the full file name.
-                        $EnvironmentSettings = "$($EnvironmentSettings)_settings.xml"
-                    }
-                    if (!(Test-Path -Path $EnvironmentSettings)) {
-                        $DeploymentToolsPath = Get-ChildItem -Path $ApplicationPath -Recurse -Filter 'DeployTools' | Select-Object -ExpandProperty FullName -First 1
-                        $esxargs = [string[]]@(
-                            "`"$EnvironmentSettingsPath\\SettingsFileGenerator.xml`""
-                            "`"$EnvironmentSettingsPath`""
-                        )
-                        $exitCode = (Start-Process -FilePath "`"$DeploymentToolsPath\EnvironmentSettingsExporter.exe`"" -ArgumentList $esxargs -Wait -PassThru).ExitCode
-                        if($exitCode -ne 0) {
-                            Write-Host "##vso[task.logissue type=error;] Deploy-BTDFApplication Error while calling EnvironmentSettingsExporter, Exit Code: $exitCode"
+            if($Name -Match $CurrentWord){
+                $ApplicationPath = Join-Path $Destination $Name
+                Write-Host "Name: $Name, Environment: $Environment, Destination: $Destination, BTDeployMgmtDB: $BTDeployMgmtDB" 
+
+                if (Test-Path -Path $ApplicationPath -ErrorAction SilentlyContinue) {
+                    if ($Environment)
+                    {
+                        $EnvironmentSettingsPath = Get-ChildItem -Path $ApplicationPath -Recurse -Filter 'EnvironmentSettings' | Select-Object -ExpandProperty FullName -First 1
+                        $EnvironmentSettings = Join-Path $EnvironmentSettingsPath $Environment
+                        if ($Environment -notmatch '\.xml') {
+                            # This offers backwards compatibility for existing tasks which are set to the environment name, not the full file name.
+                            $EnvironmentSettings = "$($EnvironmentSettings)_settings.xml"
                         }
+                        if (!(Test-Path -Path $EnvironmentSettings)) {
+                            $DeploymentToolsPath = Get-ChildItem -Path $ApplicationPath -Recurse -Filter 'DeployTools' | Select-Object -ExpandProperty FullName -First 1
+                            $esxargs = [string[]]@(
+                                "`"$EnvironmentSettingsPath\\SettingsFileGenerator.xml`""
+                                "`"$EnvironmentSettingsPath`""
+                            )
+                            $exitCode = (Start-Process -FilePath "`"$DeploymentToolsPath\EnvironmentSettingsExporter.exe`"" -ArgumentList $esxargs -Wait -PassThru).ExitCode
+                            if($exitCode -ne 0) {
+                                Write-Host "##vso[task.logissue type=error;] Deploy-BTDFApplication Error while calling EnvironmentSettingsExporter, Exit Code: $exitCode"
+                            }
+                        }
+                        Get-Item -Path $EnvironmentSettings -ErrorAction Stop | Out-Null
                     }
-                    Get-Item -Path $EnvironmentSettings -ErrorAction Stop | Out-Null
-                }
 
-                $BTDFMSBuild = Get-MSBuildPath
-                $BTDFProject = Get-ChildItem -Path $ApplicationPath -Filter '*.btdfproj' -Recurse | Select-Object -ExpandProperty FullName -First 1
-                $DeployResults = Get-ChildItem -Path $ApplicationPath -Filter 'DeployResults' -Recurse | Select-Object -ExpandProperty FullName -First 1
-                $DeployResults = Join-Path $DeployResults 'DeployResults.txt'
+                    $BTDFMSBuild = Get-MSBuildPath
+                    $BTDFProject = Get-ChildItem -Path $ApplicationPath -Filter '*.btdfproj' -Recurse | Select-Object -ExpandProperty FullName -First 1
+                    $DeployResults = Get-ChildItem -Path $ApplicationPath -Filter 'DeployResults' -Recurse | Select-Object -ExpandProperty FullName -First 1
+                    $DeployResults = Join-Path $DeployResults 'DeployResults.txt'
 
-                $arguments = [string[]]@(
-                    "/l:FileLogger,Microsoft.Build.Engine;logfile=`"$DeployResults`""
-                    '/p:Configuration=Server'
-                    "/p:DeployBizTalkMgmtDB=$BTDeployMgmtDB"
-                    "/p:ENV_SETTINGS=`"$EnvironmentSettings`""
-                    "/p:SkipUndeploy=$SkipUndeploy"
-                    '/target:Deploy'
-                    "`"$BTDFProject`""
-                )
-                $cmd = $BTDFMSBuild,($arguments -join ' ') -join ' '
-                Write-Host $cmd
-                $exitCode = (Start-Process -FilePath $BTDFMSBuild -ArgumentList $arguments -NoNewWindow -Wait -PassThru).ExitCode
-                Write-Host (Get-Content -Path $DeployResults | Out-String)
-                if($exitCode -ne 0) {
-                    Write-Host ("##vso[task.logissue type=error;] Deploy-BTDFApplication error while calling MSBuild, Exit Code: {0}" -f $exitCode)
-                    Write-Host ("##vso[task.complete result=Failed;] Deploy-BTDFApplication error while calling MSBuild, Exit Code: {0}" -f $exitCode)
+                    $arguments = [string[]]@(
+                        "/l:FileLogger,Microsoft.Build.Engine;logfile=`"$DeployResults`""
+                        '/p:Configuration=Server'
+                        "/p:DeployBizTalkMgmtDB=$BTDeployMgmtDB"
+                        "/p:ENV_SETTINGS=`"$EnvironmentSettings`""
+                        "/p:SkipUndeploy=$SkipUndeploy"
+                        '/target:Deploy'
+                        "`"$BTDFProject`""
+                    )
+                    $cmd = $BTDFMSBuild,($arguments -join ' ') -join ' '
+                    Write-Host $cmd
+                    $exitCode = (Start-Process -FilePath $BTDFMSBuild -ArgumentList $arguments -NoNewWindow -Wait -PassThru).ExitCode
+                    Write-Host (Get-Content -Path $DeployResults | Out-String)
+                    if($exitCode -ne 0) {
+                        Write-Host ("##vso[task.logissue type=error;] Deploy-BTDFApplication error while calling MSBuild, Exit Code: {0}" -f $exitCode)
+                        Write-Host ("##vso[task.complete result=Failed;] Deploy-BTDFApplication error while calling MSBuild, Exit Code: {0}" -f $exitCode)
+                    } else {
+                        Write-Host "##vso[task.complete result=Succeeded;]DONE"
+                    }
                 } else {
-                    Write-Host "##vso[task.complete result=Succeeded;]DONE"
+                    Write-Host ("##vso[task.logissue type=error;] BTDF application '{0}' not found at {1}.  Deploy skipped." -f $Name,$ApplicationPath)
+                    Write-Host ("##vso[task.complete result=Failed;] BTDF application '{0}' not found at {1}.  Deploy skipped." -f $Name,$ApplicationPath)
                 }
-            } else {
-                Write-Host ("##vso[task.logissue type=error;] BTDF application '{0}' not found at {1}.  Deploy skipped." -f $Name,$ApplicationPath)
-                Write-Host ("##vso[task.complete result=Failed;] BTDF application '{0}' not found at {1}.  Deploy skipped." -f $Name,$ApplicationPath)
-            }
 
+            }
         }
     }
 }
--- BTDFUndeploy-Arti/UnDeploy-BTDFApplication.ps1
+++ BTDFUndeploy-Arti/UnDeploy-BTDFApplication.ps1
@@ -6,6 +6,12 @@ param(
 	[Parameter(Mandatory=$true,HelpMessage="In which order to run applications. String will match if it exists in name of artifact. Seperated by "","" (No spaces!). Runs left to right. Case sensetive.")]
 	[string]$RunOrder,
 
+	[Parameter(Mandatory=$true,HelpMessage="Should this run on the last node? (False = Run on all nodes not running biztalk in the cluster, True = Run on all nodes with a active biztalk service.)")]
+	[string]$ShouldRunOnLastNode='false',
+
+	[Parameter(Mandatory=$false,HelpMessage="Name of the cluster controlling bztalk. (Leave empty if target is not in cluster and set ShouldRunOnLastNode to true. This will make it run.)")]
+	[string]$ClusterName,
+
 	[string]$BTDeployMgmtDB=$true
 )
 . "$PSScriptRoot\Init-BTDFTasks.ps1"
@@ -14,6 +20,19 @@ param(
 # create an array from an arbitrary separator, in this case a space
 $RunOrderArray = $RunOrder.split(",")
 $Name = '' # Initar den tidigt då jag inte vet hur funktionen får med den.
+
+function IsLastNode {
+	if([string]::IsNullOrEmpty($ClusterName)){
+		return $true # This can be redone since this requires ShouldRunOnLastNode to be true
+	}
+	$OwnerNode = (Get-ClusterGroup -Name $ClusterName).OwnerNode
+	Write-Host $env:computername
+	Write-Host $OwnerNode
+	if($OwnerNode -eq $env:computername){
+		return $true
+	}
+	return $false
+}
 
 function Test-BTDFApplicationDeployed {
     param(
@@ -37,58 +56,63 @@ function Test-BTDFApplicationDeployed {
     }
 }
 
-$path = "$Env:AGENT_RELEASEDIRECTORY";
+[System.Convert]::ToBoolean($ShouldRunOnLastNode);
 
-$files = Get-ChildItem $path;
-for(($i = 0); $i -lt $RunOrderArray.count; $i++){
+if(IsLastNode -eq $ShouldRunOnLastNode){
 
-	$CurrentWord = $RunOrderArray[$i];
+	$path = "$Env:AGENT_RELEASEDIRECTORY";
 
-	foreach ($Name in $files){
+	$files = Get-ChildItem $path;
+	for(($i = 0); $i -lt $RunOrderArray.count; $i++){
 
-		if($Name.Contains($CurrentWord)){
-			
-			$ApplicationPath = Join-Path $Destination $Name
-			Write-Host "Name: $Name, Destination: $Destination, BTDeployMgmtDB: $BTDeployMgmtDB" 
+		$CurrentWord = $RunOrderArray[$i];
+
+		foreach ($Name in $files){
+
+			if($Name -Match $CurrentWord){
+				
+				$ApplicationPath = Join-Path $Destination $Name
+				Write-Host "Name: $Name, Destination: $Destination, BTDeployMgmtDB: $BTDeployMgmtDB" 
 
 
-			## On the server whe MgmtDB must be undeployed, check if the application is installed. On the other servers, test if the path exists.
-			if ($BTDeployMgmtDB -eq "true" -And -Not(Test-BTDFApplicationDeployed -Name $Name))
-			{
-				Write-Host ("##vso[task.logissue type=warning;] BTDF application '{0}' not in catalog.  Undeploy skipped." -f $Name)
-			}
-			else
-			{
-				if (Test-Path -Path $ApplicationPath -ErrorAction SilentlyContinue) {
-					$BTDFProject = Get-ChildItem -Path $ApplicationPath -Filter '*.btdfproj' -Recurse | Select-Object -ExpandProperty FullName -First 1
-					$DeployResults = Get-ChildItem -Path $ApplicationPath -Filter 'DeployResults' -Recurse | Select-Object -ExpandProperty FullName -First 1
-					if ($null -eq $DeployResults) {
-						Write-Host ("##vso[task.logissue type=warning;] BTDF application '{0}' not found." -f $ApplicationPath)
-					} else {
-						$DeployResults = Join-Path $DeployResults 'DeployResults.txt'
-
-						$BTDFMSBuild = Get-MSBuildPath
-						$arguments = [string[]]@(
-							"/l:FileLogger,Microsoft.Build.Engine;logfile=`"$DeployResults`""
-							"/p:Configuration=Server"
-							"/p:DeployBizTalkMgmtDB=$BTDeployMgmtDB"
-							'/target:Undeploy'
-							"""$BTDFProject"""
-						)
-						$cmd = $BTDFMSBuild,($arguments -join ' ') -join ' '
-						Write-Host $cmd
-						$exitCode = (Start-Process -FilePath "$BTDFMSBuild" -ArgumentList $arguments -Wait -PassThru).ExitCode
-						Write-Host (Get-Content -Path $DeployResults | Out-String)
-
-						if($exitCode -ne 0) {
-							Write-Host "##vso[task.logissue type=error;] Error while calling MSBuild, Exit Code: $exitCode"
-							Write-Host ("##vso[task.complete result=Failed;] Undeploy-BTDFApplication error while calling MSBuild, Exit Code: {0}" -f $exitCode)
+				## On the server whe MgmtDB must be undeployed, check if the application is installed. On the other servers, test if the path exists.
+				if ($BTDeployMgmtDB -eq "true" -And -Not(Test-BTDFApplicationDeployed -Name $Name))
+				{
+					Write-Host ("##vso[task.logissue type=warning;] BTDF application '{0}' not in catalog.  Undeploy skipped." -f $Name)
+				}
+				else
+				{
+					if (Test-Path -Path $ApplicationPath -ErrorAction SilentlyContinue) {
+						$BTDFProject = Get-ChildItem -Path $ApplicationPath -Filter '*.btdfproj' -Recurse | Select-Object -ExpandProperty FullName -First 1
+						$DeployResults = Get-ChildItem -Path $ApplicationPath -Filter 'DeployResults' -Recurse | Select-Object -ExpandProperty FullName -First 1
+						if ($null -eq $DeployResults) {
+							Write-Host ("##vso[task.logissue type=warning;] BTDF application '{0}' not found." -f $ApplicationPath)
 						} else {
-							Write-Host "##vso[task.complete result=Succeeded;]DONE"
+							$DeployResults = Join-Path $DeployResults 'DeployResults.txt'
+
+							$BTDFMSBuild = Get-MSBuildPath
+							$arguments = [string[]]@(
+								"/l:FileLogger,Microsoft.Build.Engine;logfile=`"$DeployResults`""
+								"/p:Configuration=Server"
+								"/p:DeployBizTalkMgmtDB=$BTDeployMgmtDB"
+								'/target:Undeploy'
+								"""$BTDFProject"""
+							)
+							$cmd = $BTDFMSBuild,($arguments -join ' ') -join ' '
+							Write-Host $cmd
+							$exitCode = (Start-Process -FilePath "$BTDFMSBuild" -ArgumentList $arguments -Wait -PassThru).ExitCode
+							Write-Host (Get-Content -Path $DeployResults | Out-String)
+
+							if($exitCode -ne 0) {
+								Write-Host "##vso[task.logissue type=error;] Error while calling MSBuild, Exit Code: $exitCode"
+								Write-Host ("##vso[task.complete result=Failed;] Undeploy-BTDFApplication error while calling MSBuild, Exit Code: {0}" -f $exitCode)
+							} else {
+								Write-Host "##vso[task.complete result=Succeeded;]DONE"
+							}
 						}
+					} else {
+						Write-Host ("##vso[task.logissue type=warning;] BTDF application '{0}' not found at {1}.  Undeploy skipped." -f $Name,$ApplicationPath)
 					}
-				} else {
-					Write-Host ("##vso[task.logissue type=warning;] BTDF application '{0}' not found at {1}.  Undeploy skipped." -f $Name,$ApplicationPath)
 				}
 			}
 		}
